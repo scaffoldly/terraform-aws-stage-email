3c98bc68e2c969b020db4fa58bde63b6c597f0f4
Christian Nuss <christian.nuss@gmail.com>
2021-09-07 15:22:46 -0700

    Add a . to the zone data

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
 data "aws_route53_zone" "zone" {
-  name     = var.domain
+  name = "${var.domain}."
+
   provider = aws.dns
 }
 
